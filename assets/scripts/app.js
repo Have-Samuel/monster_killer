cdeee0ff02050159992c67190b03caf77299a73f
Have Samuel <samiehave9@gmail.com>
2022-11-16 11:58:06 +0300

    Using the For in Loop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -223,7 +223,7 @@ function healPlayerHander() {
   );
   endRound();
 }
-
+// FOR LOOPS
 function printLogHandler() {
   // for (let i = 0; i < 3; i++) {
   //   console.log('-------');
@@ -237,8 +237,10 @@ function printLogHandler() {
   // }
   let i = 0;
   for (const logEntry of battleLog) {
-    console.log(logEntry);
-    console.log(i);
+    console.log(`#${i}`);
+    for (const key in logEntry) {
+      console.log(`${key} => ${logEntry[key]}`);
+    }
     i++;
   }
 }
